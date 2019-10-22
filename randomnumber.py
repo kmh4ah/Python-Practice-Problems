dc3feb1653a789987c20df93c79d542480e979a3
Khin Maung Htay <bestchoice.kmh@gmail.com>
2019-10-22 09:01:26 +0800

    Create randomnumber.py
    Generating a random number

---
+++ randomnumber.py
@@ -0,0 +1,7 @@
+
+############## GENERATE A RANDOM NUMBER ####################################
+import random 
+random_num = random.randint(0,10)
+print("Random Number Generated: ", random_num)
+
+print("\n")
