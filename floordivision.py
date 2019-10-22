464f32060a0750f5ba95b276ab6265298117c497
Khin Maung Htay <bestchoice.kmh@gmail.com>
2019-10-22 09:11:55 +0800

    Create floordivision.py

---
+++ floordivision.py
@@ -0,0 +1,14 @@
+############################## FIND FLOOR DIVISION ####################################
+print("This program can find floor division between the two numbers")
+
+f_num = int(input("Enter the first number: ")
+s_num = int(input("Enter the second number: ")
+
+def f_division(first,second):
+  floor = f_num // s_num
+  return "The floor division is: " + str(floor)
+floor = f_division(f_num,s_num)
+print(floor)
+
+print("\n")
+
